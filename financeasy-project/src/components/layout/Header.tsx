4a756fec2dc00de98a6f94c3b1d2be959bf3ce54
Malu Abrami <marialuizaabrami@gmail.com>
2026-01-30 20:56:36 -0300

    feat: estilizando hover da navegação na header

--- financeasy-project/src/components/layout/Header.tsx
+++ financeasy-project/src/components/layout/Header.tsx
@@ -8,10 +8,54 @@ export function Header() {
                     <a href="" className="text-4xl">Financeasy</a>
 
                     <ul className="flex gap-5">
-                        <li><a href="">Início</a></li>
-                        <li><a href="">Sobre</a></li>
-                        <li><a href="">Serviços</a></li>
-                        <li><a href="">Contato</a></li>
+                        <li className="relative group">
+                            <a href="#" className="text-slate-300">
+                            Início
+                            </a>
+                            <span className="
+                            absolute left-0 -bottom-1
+                            h-[2px] w-0
+                            bg-sky-400
+                            transition-all duration-300
+                            group-hover:w-full
+                            " />
+                        </li>
+                        <li className="relative group">
+                            <a href="" className="text-slate-300">
+                                Sobre
+                            </a>
+                            <span className="
+                            absolute left-0 -bottom-1
+                            h-[2px] w-0
+                            bg-sky-400
+                            transition-all duration-300
+                            group-hover:w-full
+                            " />
+                        </li>
+                        <li className="relative group">
+                            <a href="" className="text-slate-300">
+                                Serviços
+                            </a>
+                            <span className="
+                            absolute left-0 -bottom-1
+                            h-[2px] w-0
+                            bg-sky-400
+                            transition-all duration-300
+                            group-hover:w-full
+                            " />
+                        </li>
+                        <li className="relative group">
+                            <a href="" className="text-slate-300">
+                                Contato
+                            </a>
+                            <span className="
+                            absolute left-0 -bottom-1
+                            h-[2px] w-0
+                            bg-sky-400
+                            transition-all duration-300
+                            group-hover:w-full
+                            " />
+                        </li>
                     </ul>
 
                     <div className="flex gap-5">
